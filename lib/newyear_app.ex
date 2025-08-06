3765c419f58d2e702e6e4a7b4231d507be14704b
Emma <emma.malinsdotter@outlook.com>
2025-08-06 15:04:35 +0200

    la till dagens, gårdagens och morgondagens datum

--- lib/newyear_app.ex
+++ lib/newyear_app.ex
@@ -10,6 +10,11 @@ defmodule NewyearApp do
   def main do
     target = Timex.to_datetime({{2026, 1, 1}, {0, 0, 0}}, "Europe/Stockholm")
     now = Timex.now("Europe/Stockholm")
+	today = Timex.to_date(now)
+	yesterday = Timex.shift(now, days: -1)
+    yesterday_date = Timex.to_date(yesterday)
+	tomorrow = Timex.shift(now, days: +1)
+    tomorrows_date = Timex.to_date(tomorrow)
 
     time_till = DateTime.diff(target, now)
 
@@ -18,6 +23,10 @@ defmodule NewyearApp do
     minutes = div(rem(time_till, 3_600), 60)
     seconds = rem(time_till, 60)
 
+
+    IO.puts("Dagens datum i Sverige: #{today}")
+	IO.puts("Gårdagens datum i Sverige: #{yesterday_date}")
+	IO.puts("Morgondagens datum i Sverige: #{tomorrows_date}")
     IO.puts("Time until the New Year: #{days} days, #{hours} hours, #{minutes} minutes, #{seconds} seconds.")
   end
 end
